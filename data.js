c92f5bbe0b255272f3c9a8eea18e23a032654d50
Marcelo Borges <106216446+marcelobps@users.noreply.github.com>
2026-02-11 16:56:30 -0300

    Update data.js

--- data.js
+++ data.js
@@ -19,35 +19,167 @@ const candidatos = [
 
     // Theo teste... (excluir depois)
     { 
-        numero: '10', 
+        numero: '00', 
         nome: 'Theo', 
         turma: '6º Ano A', 
         foto: 'img/foto_0.jpg' // Foto do Theo renomeada
     }, 
 
-    // 6º Ano A
+    // 1ª Série A
     { 
-        numero: '11', 
-        nome: 'Ana Silva', 
-        turma: '6º Ano A', 
+        numero: '01', 
+        nome: 'ANA CAROLINA MIGUEL BERNADES', 
+        turma: '1ª Série A', 
         foto: 'img/default_girl.png' // Caminho relativo
     },
     { 
-        numero: '12', 
-        nome: 'Carla Souza', 
-        turma: '6º Ano A', 
+        numero: '02', 
+        nome: 'BIANCA HENRIQUE BARBOSA', 
+        turma: '1ª Série A', 
+        foto: 'img/default_girl.png' 
+    },
+    { 
+        numero: '14', 
+        nome: 'KAIO DE REZENDE GALVÃO', 
+        turma: '1ª Série A', 
         foto: 'img/default_girl.png' 
     },
     
     // Para alunos sem foto, você pode manter um link genérico ou usar uma imagem padrão:
     //                                      img/default_boy.png ou /img/default_girl.png
+    // 1ª Série B
     { 
-        numero: '20', 
-        nome: 'Daniel Alves', 
-        turma: '6º Ano B', 
+        numero: '05', 
+        nome: 'DANIEL ELIAS AMARAL ROCHA', 
+        turma: '1ª Série B', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '21', 
+        nome: 'MOYSES FELIPE HATHENHER', 
+        turma: '1ª Série B', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '24', 
+        nome: 'NIRIELLE MENDES DA TRINDADE', 
+        turma: '1ª Série B', 
         foto: 'img/default_boy.png' 
     },
 
+    // 1ª Série C
+    { 
+        numero: '16', 
+        nome: 'JOÃO GABRIEL RIBEIRO ASSUNÇÃO', 
+        turma: '1ª Série C', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '18', 
+        nome: 'JULIA NEVES OLIVEIRA', 
+        turma: '1ª Série C', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '23', 
+        nome: 'MARIA GABRIELA SOUZA LEMOS', 
+        turma: '1ª Série C', 
+        foto: 'img/default_boy.png' 
+    },
+
+    // 2º AUTOMAÇÃO
+    { 
+        numero: '22', 
+        nome: 'LAURA ZANINI DE FREITAS PONTES', 
+        turma: '2º Automação', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '25', 
+        nome: 'MARIA LUÍSA ROSA MARTIN', 
+        turma: '2º Automação', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '28', 
+        nome: 'MATEUS RECH JOANUCCI', 
+        turma: '2º Automação', 
+        foto: 'img/default_boy.png' 
+    },
+
+    // 2º MINERAÇÃO
+    { 
+        numero: '15', 
+        nome: 'GABRIEL RAMALHO LIMA', 
+        turma: '2º Mineração', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '33', 
+        nome: 'MARIANNA CRISTINA FONSECA', 
+        turma: '2º Mineração', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '35', 
+        nome: 'PYETRA MONTEIRO VAZ', 
+        turma: '2º Mineração', 
+        foto: 'img/default_boy.png' 
+    },
+
+    // 2º ELETROMECÂNICA
+    { 
+        numero: '03', 
+        nome: 'CALISMAR REBEIRO FILHO', 
+        turma: '2º Eletromecânica', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '10', 
+        nome: 'JOÃO PEDRO NORONHA SANTOS', 
+        turma: '2º Eletromecânica', 
+        foto: 'img/default_boy.png' 
+    },
+
+    // 3º IOT
+    { 
+        numero: '32', 
+        nome: 'THIAGO DA SILVA ALMEIDA', 
+        turma: '3º Internet das Coisas', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '26', 
+        nome: 'MATHEUS PASCOAL DE OLIVEIRA', 
+        turma: '3º Internet das Coisas', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '29', 
+        nome: 'MIGUEL PEREIRA DA SILVA', 
+        turma: '3º Internet das Coisas', 
+        foto: 'img/default_boy.png' 
+    },
+
+    // 3º MINERAÇÃO
+    { 
+        numero: '03', 
+        nome: 'BRENNO KAYKY ARAUJO', 
+        turma: '3º Mineração', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '08', 
+        nome: 'ERICK VINICIUS DOS SANTOS', 
+        turma: '3º Mineração', 
+        foto: 'img/default_boy.png' 
+    },
+    { 
+        numero: '27', 
+        nome: 'RAQUEL HELENA GALDINO', 
+        turma: '3º Mineração', 
+        foto: 'img/default_boy.png' 
+    },
     { 
         // adicione outros aqui...
     },
